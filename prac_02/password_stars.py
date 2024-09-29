51b955850dca46358eccf5190e4feb6098015ac8
Yuchen Liu <yuchen.liu2@my.jcu.edu.au>
2024-09-29 14:02:46 +0800

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,19 @@
-min_length = 8
-password = input("Please enter your password: ")
-while len(password) < min_length:
+def main():
+ min_length = 8
+ password = get_password()
+ while len(password) < min_length:
     print(f"The password should be at least {min_length} characters long.")
     password = input("Please enter again: ")
-print("*" * len(password))
+ print_star(password)
+
+
+def print_star(password):
+    print("*" * len(password))
+
+
+def get_password():
+    password = input("Please enter your password: ")
+    return password
+
+
+main()
