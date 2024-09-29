5cbb40dbafd957ce4199c57ecab26feebcde6428
Yuchen Liu <yuchen.liu2@my.jcu.edu.au>
2024-09-29 14:49:16 +0800

    Modify score program

--- prac_01/broken_score.py
+++ prac_01/broken_score.py
@@ -4,14 +4,11 @@ Broken program to determine score status
 """
 
 score = float(input("Enter score: "))
-if score < 0 :
+if score < 0 or score >100:
     print("Invalid score")
-else:
-    if score >= 100:
-        print("Invalid score")
-    if score >= 50:
-        print("Passable")
-    if score >= 90:
+elif  score >= 90:
      print("Excellent")
-if score < 50:
-    print("Bad")
+elif  score >= 50:
+      print("Passable")
+else:
+     print("Bad")
