2b2c348b100c43a884992aa75d8ee000398fc910
Yuchen Liu <yuchen.liu2@my.jcu.edu.au>
2024-11-02 15:33:49 +0800

    Print a file as expected

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -11,19 +11,14 @@ def main():
     """Demo test code to show how to use car class."""
     my_car = Car(180)
     my_car.drive(30)
-    limo = Car(100)
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
-    print(f"Car has fuel: {my_car.fuel}")
 
-    limo.add(20)
-    print(f"Limo after adding fuel has fuel: {limo.fuel}")
-    print(limo)
-
-    print(f"Limo's fuel amount:{limo.fuel}")
+    limo = Car(100)
+    limo.add_fuel(20)
+    print(f"Limo's fuel is: {limo.fuel}")
 
     limo.drive(115)
-    print(f"Limo after driving 115 kilometers has fuel: {limo.fuel}")
     print(limo)
 
 
