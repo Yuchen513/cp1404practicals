6cfd9d80c8bceda43e664f8f87d9b76970c178dd
Yuchen Liu <yuchen.liu2@my.jcu.edu.au>
2024-12-02 17:54:11 +0800

    Add a disambiguating error

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -5,8 +5,18 @@ def main():
 
     page_title = input("Input the page title: ")
     while page_title != " ":
+        try:
             page = wikipedia.page(page_title, auto_suggest=False)
             print(page.title)
             print(page.summary.strip())
             print(page.url)
+
+        except wikipedia.DisambiguationError:
+            # Disambiguation
+            print("We need a more specific title. Try one of the following, or a new search:")
+            print(wikipedia.search(page_title))
+
+
+
+
 main()
